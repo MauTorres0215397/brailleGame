4a533431e3d9c220c1e0f063d39b53e6e87fb65e
MauTorres0215397 <74576948+MauTorres0215397@users.noreply.github.com>
2022-08-04 19:36:49 -0500

    agregar modo
    Se agregó el modo formando palabras, una combinación de los dos modos anteriores

--- js/main.js
+++ js/main.js
@@ -96,17 +96,40 @@ let firstChar = true;
 
 // braille display word
 let brailleWord = document.querySelector('.braille-dots')
-let words = ['perro', 'pastor', 'braille', 'puntos', 'ceguera', 'ciego', 'libro', 'poder', 'puntos', 'bastones', 'vidente', 'signo'];
+let words = ['abadíes', 'acierto', 'adoptas', 'afianza', 'aflojar', 'agendas', 'ajustes', 'anillos', 'antenas', 'aplasto',
+    'aplique', 'arbitro', 'ardilla', 'arrojar', 'arrugas', 'balones', 'barrios', 'bazares', 'bestias', 'biblias',
+    'bloques', 'bonitos', 'botadas', 'botando', 'boscosa', 'boletas', 'banales', 'avispas', 'atisbes', 'idiomas',
+    'idiotez', 'integra', 'infecta', 'juguito', 'kimonos', 'labores', 'lagañas', 'lanoso', 'lenguas', 'llueven',
+    'lunares', 'motivas', 'muertos', 'navales', 'panales', 'papayas', 'papeles', 'parques', 'patrias', 'pecados',
+    'pañales', 'pruebas', 'relojes', 'renales', 'rivales', 'romanos', 'rurales', 'palabra', 'palabra', 'vacunas',
+    'valores', 'varones', 'veremos', 'vinagre', 'vocales', 'vomitas', 'yanquis', 'yunques', 'zarinas', 'zullona',
+    'william', 'wyoming'];
+
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let making = []
+
 words = words.sort(() => Math.random() - 0.5);
+
 letters = letters.sort(() => Math.random() - 0.5);
 
+let rand = Math.floor(Math.random() * (words.length));
+let makingWord = words[rand];
+for (var i = 0; i < makingWord.length - 1; i++) {
+    making.push(makingWord[i]);
+}
+making.push(makingWord[makingWord.length - 1])
+making.push(makingWord);
+console.log(words);
+console.log(letters);
+console.log(making);
+console.log(makingWord.length)
+
 let input = document.querySelector('input');
-let activeSet = true;
+let activeSet = 'words';
 
 window.addEventListener('DOMContentLoaded', (event) => {
     // display the word from the active set
-    brailleWord.innerHTML = (activeSet) ? words[0] : letters[0];
+    brailleWord.innerHTML = selectMode(activeSet);
 
     // everytime the user writes something 
     input.addEventListener('input', (event) => {
@@ -122,10 +145,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
         if (event.key == 'Enter') {
             // add element to list if the input and the braille word displayed are the same
             if (input.value == brailleWord.innerHTML) {
+                console.log('palabra correcta')
                 resetInput();
                 add2Chart(input.value);
-                brailleWord.innerHTML = (activeSet) ? words[words.indexOf(brailleWord.innerHTML) + 1] : letters[letters.indexOf(brailleWord.innerHTML) + 1];
+                let temp = '';
+                switch (activeSet) {
+                    case 'words':
+                        temp = words[words.indexOf(brailleWord.innerHTML) + 1];
+                        console.log(activeSet);
+                        break;
+                    case 'letters':
+                        temp = letters[letters.indexOf(brailleWord.innerHTML) + 1];
+                        console.log(activeSet);
+                        break;
+                    case 'making':
+                        let indexTemp = making.indexOf(brailleWord.innerHTML) + 1;
+                        console.log(indexTemp + ' ' + making.length);
+                        temp = (indexTemp == making.length) ? making[0] : making[making.indexOf(brailleWord.innerHTML) + 1];
+                        console.log(temp);
+                        break;
+                }
+                brailleWord.innerHTML = temp;
             } else {
+                console.log(input.value + ' ' + brailleWord.innerHTML)
                 input.style.backgroundColor = '#F6CCD0';
                 input.classList.add('border-danger');
             }
@@ -153,12 +195,32 @@ function changeSet() {
 }
 function wordArray() {
     changeSet();
+    activeSet = 'words';
     brailleWord.innerHTML = words[0];
     chart.innerHTML = '';
 
 }
 function letterArray() {
     changeSet();
+    activeSet = 'letters';
     brailleWord.innerHTML = letters[0];
     chart.innerHTML = '';
+}
+
+function makingWordsMode() {
+    changeSet();
+    activeSet = 'making';
+    brailleWord.innerHTML = making[0];
+    chart.innerHTML = '';
+}
+
+function selectMode(mode) {
+    switch (mode) {
+        case 'words':
+            return words[0];
+        case 'letters':
+            return letters[0];
+        case 'making':
+            return making[0];
+    }
 }
